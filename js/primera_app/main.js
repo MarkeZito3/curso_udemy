e848106ad8030e7a5ff81cf1cc4163a3b61b4eb7
MarkeZito3 <MarkkesGames@hotmail.com>
2022-07-15 14:57:10 -0300

    use of .map and .join in an array

--- js/primera_app/main.js
+++ js/primera_app/main.js
@@ -13,9 +13,15 @@ window.onload = () => {
         console.log(todos);
         
         const todoList = document.getElementById("todo-list") // tomamos el ul que está en el HTML para  poder agregar más listas de todos 
-        todoList.innerHTML = ""; // vacío el HTML de todolist 
-        for(let i = 0; i < todos.length; i++) {
-            todoList.innerHTML += "<li>" + todos[i] + "</li>"; //agrego un <li> para mostrar la la lista
-        };
+
+        // hay una mejor manera de hacer lo que está escrito acá abajo, y es con map
+        // todoList.innerHTML = ""; // vacío el HTML de todolist 
+        // for(let i = 0; i < todos.length; i++) {
+        //     todoList.innerHTML += "<li>" + todos[i] + "</li>"; //agrego un <li> para mostrar la la lista
+        // };
+
+        const todosTemplate = todos.map(t => '<li>' + t + '</li>'); //como parámetro se coloca un elemento que vamos a estar iterando
+        console.log(todosTemplate);
+        todoList.innerHTML = todosTemplate.join(""); // tomar todos los elementos de un arreglo y juntarlos mediante lo que esté escrito dentro de los paréntesis 
     };
 }
