4bf67ff88b23df1d3e989d7f61d9129efa4ca9e2
MarkeZito3 <MarkkesGames@hotmail.com>
2022-07-17 10:09:41 -0300

    I add a render with recursivity to render the array called 'todos'

--- js/primera_app/main.js
+++ js/primera_app/main.js
@@ -1,5 +1,23 @@
 const todos = []; // esto es un arreglo vacio para colocar todos los todo como memoria
 
+const render = () => {
+    const todoList = document.getElementById("todo-list") // tomamos el ul que está en el HTML para  poder agregar más listas de todos 
+    const todosTemplate = todos.map(t => '<li>' + t + '</li>'); //como parámetro se coloca un elemento que vamos a estar iterando
+    console.log(todosTemplate);
+    todoList.innerHTML = todosTemplate.join(""); // tomar todos los elementos de un arreglo y juntarlos mediante lo que esté escrito dentro de los paréntesis 
+    const todoList_li = document.querySelectorAll("#todo-list li") //esto busca todos los elementos | para usar los ID se ocupa el #
+    // Array.from(todoList_li) //Esto es para poder transformar lo de arriba a un array
+
+    todoList_li.forEach((todoList_li, i) => { // esto sirve para iterar, al igual que .map (con la diferencia que .forEach no se puede colocar a una constante)
+        todoList_li.addEventListener("click",() => { // este evento detecta cuando el usuario hace click por encima del <li> con la ID que detectó anteriormente
+            todoList_li.parentNode.removeChild(todoList_li); //parentNode transforma al elemento que es el padre (esto se hace ya que los nodos padres son los que pueden eliminar a sus hijos)
+            todos.splice(i, 1); // con splice se elimina un elemento del array .splice(indice,cantidad_de_elementos)
+            render();
+            console.log("Se eliminó:", todoList_li,todos,i);
+        })
+    })
+}
+
 window.onload = () => { 
     const form = document.getElementById("todo-form"); // se obtiene la referencia con getElementById
     form.onsubmit = (e) => { // reemplazamos la funcion que tenía en onsubmit
@@ -12,16 +30,13 @@ window.onload = () => {
         todos.push(todoText); // agrego contenido al arreglo de todos
         console.log(todos);
         
-        const todoList = document.getElementById("todo-list") // tomamos el ul que está en el HTML para  poder agregar más listas de todos 
-
+        
         // hay una mejor manera de hacer lo que está escrito acá abajo, y es con map
         // todoList.innerHTML = ""; // vacío el HTML de todolist 
         // for(let i = 0; i < todos.length; i++) {
         //     todoList.innerHTML += "<li>" + todos[i] + "</li>"; //agrego un <li> para mostrar la la lista
         // };
-
-        const todosTemplate = todos.map(t => '<li>' + t + '</li>'); //como parámetro se coloca un elemento que vamos a estar iterando
-        console.log(todosTemplate);
-        todoList.innerHTML = todosTemplate.join(""); // tomar todos los elementos de un arreglo y juntarlos mediante lo que esté escrito dentro de los paréntesis 
-    };
+        
+        render()
+    }
 }
